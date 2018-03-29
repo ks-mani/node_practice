ecfdd5d6356ce51134c5fb4b71bc482820a59465
ksmani <kumar.mani017@gmail.com>
2018-03-29 22:46:35 +0530

    Section3: Part5: Parsing Headers

--- app/index.js
+++ app/index.js
@@ -24,11 +24,15 @@ var server = http.createServer(function(req, res){
   // Get the HTTP method
   var method = req.method.toLowerCase();
 
+  // Get the headers as an object
+  var headers = req.headers;
+
   //Send the response
   res.end('Hello World\n');
 
   // Log the request path
   console.log('Request received on this path: '+trimmedPath+' with method: '+method+' and with these query string parameters: ', queryStringObject);
+  console.log('Request received with these headers', headers);
 });
 
 // Start the server, and have it listen on port 3000
