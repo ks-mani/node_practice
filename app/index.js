b63921054ab0870d526aa446995b925ee51f85f0
ksmani <kumar.mani017@gmail.com>
2018-03-29 22:05:56 +0530

    Section 3: Part2: Trimmed URL

--- app/index.js
+++ app/index.js
@@ -6,10 +6,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
+
+  //Get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  //Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  //Send the response
+
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request received on this path: '+trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
