1fe7635f8a23629fc58db169fd8b3e07c432d8db
ksmani <kumar.mani017@gmail.com>
2018-03-29 23:35:34 +0530

    Section3: Part5: PArsing Payload

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
@@ -27,12 +28,26 @@ var server = http.createServer(function(req, res){
   // Get the headers as an object
   var headers = req.headers;
 
+  // Get the payload, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+
+  req.on('data', function(data){
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function(){
+    buffer += decoder.end();
+
+    // Log the request path
+    console.log('Request received on this path: '+trimmedPath+' with method: '+method+' and with these query string parameters: ', queryStringObject);
+    console.log('Request received with these headers: ', headers);
+
+    console.log('Request received with this payload: ', buffer);
+  });
+
   //Send the response
   res.end('Hello World\n');
-
-  // Log the request path
-  console.log('Request received on this path: '+trimmedPath+' with method: '+method+' and with these query string parameters: ', queryStringObject);
-  console.log('Request received with these headers', headers);
 });
 
 // Start the server, and have it listen on port 3000
